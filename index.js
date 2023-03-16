74863585c77f907612948dd92343f12e1e8b649b
dipratnasakhare <dipratnasakhare11@email.com>
2023-03-16 07:49:48 +0530

    adding json

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
 const port = 4000
+const cors = require("cors");
 
+
+app.use(cors())
 server.use(middlewares)
 server.use(router)
 server.listen(port)
